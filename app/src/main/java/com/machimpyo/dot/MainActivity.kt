138984c3a13599d49f5d6ff0cff40741a9712eb6
JMS1208 <wjsalstjr59@gmail.com>
2023-07-17 01:05:34 +0900

    :heavy_plus_sign: Screen Navigation Settings with Nav-Animation

--- app/src/main/java/com/machimpyo/dot/MainActivity.kt
+++ app/src/main/java/com/machimpyo/dot/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.machimpyo.dot.navigation.AppNavHost
 import com.machimpyo.dot.ui.theme.MachimpyoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppNavHost()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MachimpyoTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/machimpyo/dot/navigation/AppNavHost.kt
@@ -0,0 +1,70 @@
+package com.machimpyo.dot.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberAnimatedNavController(),
+    startDestination: String = ROUTE_SPLASH
+) {
+    AnimatedNavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+
+        /*
+        아래는 예시코드에요! (지울 예정)
+         */
+        composable(
+            route = ROUTE_SPLASH
+        ) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize()
+            ) { innerPadding->
+                Box(
+                    modifier = Modifier.padding(innerPadding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("스플래쉬 화면")
+                }
+            }
+        }
+
+        /*
+        https://google.github.io/accompanist/navigation-animation/ 참고
+         */
+//        composable(
+//            route = ROUTE_SPLASH,
+//            enterTransition = {
+//
+//            },
+//            exitTransition = {
+//
+//            },
+//            popEnterTransition = {
+//
+//            },
+//            popExitTransition = {
+//
+//            }
+//        ) {
+//
+//        }
+    }
+}
