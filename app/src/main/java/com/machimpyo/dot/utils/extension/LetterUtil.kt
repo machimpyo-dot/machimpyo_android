0b9c0bb750eabca780823369e9ef574f59afc098
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-08-25 11:19:17 +0900

    + 편지지 디자인 관련 Util

---
+++ app/src/main/java/com/machimpyo/dot/utils/extension/LetterUtil.kt
@@ -0,0 +1,34 @@
+package com.machimpyo.dot.utils.extension
+
+import androidx.compose.ui.graphics.Color
+import com.machimpyo.dot.R
+
+val LetterColorList: List<Color> = listOf<Color>(
+    Color(0XFF5CBDF1),
+    Color(0XFF37B9FF),
+    Color(0XFF007FF4),
+    Color(0XFF6CE2F2),
+    Color(0XFF36CEE3),
+    Color(0XFF00AFE7),
+    Color(0XFFA5C9FF),
+    Color(0XFF7AAFFF),
+//        Color(0XFFFF8E3D),
+//        Color(0XFFFFA337),
+//        Color(0XFFFF7337),
+//        Color(0XFFFFC9B1),
+    Color(0XFFFFC28A),
+    Color(0XFFFFC148),
+    Color(0XFFFFE6CF),
+    Color(0XFFFFCE70),
+    Color(0XFFFF8A00),
+)
+
+
+val LetterPatternList = listOf(
+    R.drawable.letter_pattern_0,
+    R.drawable.letter_pattern_1,
+    R.drawable.letter_pattern_2,
+    R.drawable.letter_pattern_3,
+    R.drawable.letter_pattern_4,
+    R.drawable.letter_pattern_5,
+)
